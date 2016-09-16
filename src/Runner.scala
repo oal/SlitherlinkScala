87d614d274d97ccec7513f259502801347a91edb
Olav Lindekleiv <olav@lindekleiv.com>
2016-09-16 18:53:08 +0200

    Remove incorrect puzzle and make SolutionPrinter output board to file instead of stdout.

--- src/Runner.scala
+++ src/Runner.scala
@@ -1,19 +1,24 @@
-import java.io.File
+import java.io.{File, PrintWriter}
 import java.util.Calendar
 
 object Runner extends App {
   val dir = new File("input")
-  dir.listFiles().foreach(file => {
-    println(s"Loading boards from ${file.getAbsolutePath()}")
+  dir.listFiles().filter(file => file.getName.head != '.').foreach(file => {
+    println(s"Loading boards from ${file.getAbsolutePath}")
     val parser = new BoardParser(file)
+
+    val out = new PrintWriter(new File(s"output/${file.getName}"), "UTF-8")
+    out.println(parser.numPuzzles)
 
     println(s"Found ${parser.boards.length} boards. Solving...")
     parser.boards.foreach(board => {
       val start = Calendar.getInstance().getTimeInMillis
       new Solver(board).solve()
-      new SolutionPrinter(board)
+      SolutionPrinter.printBoard(board, out)
       println(s"Solved ${board.row.length}x${board.row.length} board in ${Calendar.getInstance().getTimeInMillis - start}ms")
     })
+
+    out.close()
   })
 
 }
--- src/SolutionPrinter.scala
+++ src/SolutionPrinter.scala
@@ -1,31 +1,35 @@
-class SolutionPrinter(board: Board) {
-  for (y <- board.row) {
-    for (x <- y.square) {
-      print("+" + getHorSymbol(x.y, x.x, 'Up))
+import java.io.PrintWriter
+
+object SolutionPrinter {
+  def printBoard(board: Board, out: PrintWriter) = {
+    out.println(s"${board.row.head.square.length}x${board.row.length}")
+    for (y <- board.row) {
+      for (x <- y.square) {
+        out.print("+" + getHorSymbol(x.y, x.x, 'Up))
+      }
+      out.println("+")
+      for (x <- y.square) {
+        out.print(getVerSymbol(x.y, x.x, 'Left) + " ")
+      }
+      out.println(getVerSymbol(y.square.last.y, y.square.last.x, 'Right))
     }
-    println("+")
-    for (x <- y.square) {
-      print(getVerSymbol(x.y, x.x, 'Left) + " ")
+    for (x <- board.row.last.square) {
+      out.print("+" + getHorSymbol(x.y, x.x, 'Down))
     }
-    println(getVerSymbol(y.square.last.y, y.square.last.x, 'Right))
-  }
-  for (x <- board.row.last.square) {
-    print("+" + getHorSymbol(x.y, x.x, 'Down))
-  }
-  println("+")
+    out.println("+")
 
-  def getHorSymbol(y: Int, x: Int, dir: Symbol): String = {
-    if (board.row(y).square(x).connector(dir).set)
-      "-"
-    else
-      " "
-  }
+    def getHorSymbol(y: Int, x: Int, dir: Symbol): String = {
+      if (board.row(y).square(x).connector(dir).set)
+        "-"
+      else
+        " "
+    }
 
-  def getVerSymbol(y: Int, x: Int, dir: Symbol): String = {
-    if (board.row(y).square(x).connector(dir).set)
-      "|"
-    else
-      " "
+    def getVerSymbol(y: Int, x: Int, dir: Symbol): String = {
+      if (board.row(y).square(x).connector(dir).set)
+        "|"
+      else
+        " "
+    }
   }
-
 }
